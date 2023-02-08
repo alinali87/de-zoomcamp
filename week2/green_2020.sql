8a31fd6402f06a22e23fc35b6a384deefaaf2d51
Alina Li <alinali87@gmail.com>
2023-02-08 23:04:57 +0400

    week2

--- week2/green_2020.sql
+++
@@ -1,5 +0,0 @@
--- How many rows does that dataset have?
-SELECT count(*) FROM `lustrous-method-376106.trips_data_all.green_2020`
-
--- Question 4. Github Storage Block
-SELECT count(*) FROM `lustrous-method-376106.trips_data_all.green_2020_11`
